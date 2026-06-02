edbf44c51fe06825f8d39b9326bf80c1d4cd0455
ikirtvardhansingh <ikirtivardhansingh@gmail.com>
2026-06-02 10:50:04 +0530

    P2 Section 3 : Some avgs

--- src/eda.js
+++ src/eda.js
@@ -11,9 +11,24 @@ function analyzeData(data){
     console.log("Total Churned Customers:", totalChurns);
     const ChurnedPerc = churnRate(totalCustomers, totalChurns).toFixed(2);
     console.log("Churn Rate:", ChurnedPerc + "%");
+    avgTenure(data, totalCustomers);
+    avgMonthCharges(data, totalCustomers);
 }
+
 function churnRate(tc, churned){
     return (churned/ tc) * 100;
+}
+
+function avgTenure(data, tc){
+  const avegTenure =  data.reduce((acc, customer) => acc + Number(customer.tenure), 0)/tc;
+    
+  console.log("avg tenure:", avegTenure.toFixed(2));
+}
+
+function avgMonthCharges(data, tc){
+    const  avgmCharges = data.reduce((acc, customer) => acc + Number(customer.MonthlyCharges), 0)/tc
+    
+    console.log("avg Monthly Charges:", avgmCharges.toFixed(2));
 }
 
 module.exports = analyzeData;
