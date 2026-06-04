9b05f2e9473d5ce8bd4b6f70ad7478e3967ad668
ikirtvardhansingh <ikirtivardhansingh@gmail.com>
2026-06-04 21:14:57 +0530

    P2: Section 4: wrapped. Added final features

--- src/preprocess.js
+++ src/preprocess.js
@@ -1,5 +1,10 @@
 function preprocessData(data){
-
+    const contract = {
+        "Month-to-month": 0,
+        "One year": 1,
+        "Two year": 2
+    };
+    
     const cleanedData = data.map(customer => {
         return { 
             tenure :        Number(customer.tenure),
@@ -7,7 +12,10 @@ function preprocessData(data){
             totalCharges :  Number(customer.TotalCharges),
             seniorCitizen : Number(customer.SeniorCitizen),
             churn :         customer.Churn === "Yes" ? 1 : 0,
-            partner :       customer.Partner === "Yes" ? 1 : 0
+            partner :       customer.Partner === "Yes" ? 1 : 0,
+            dependents :    customer.Dependents === "Yes" ? 1 : 0,
+            paperlessBilling : customer.PaperlessBilling === "Yes" ? 1 : 0,
+            contract :      contract[customer.Contract]
         };
         }
     );
@@ -16,7 +24,10 @@ function preprocessData(data){
         customer.monthlyCharges,
         customer.totalCharges,
         customer.seniorCitizen,
-        customer.partner
+        customer.partner,
+        customer.dependents,
+        customer.paperlessBilling,
+        customer.contract
     ]);
 
     const y = cleanedData.map(customer => customer.churn);
