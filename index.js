fe714380ee6c81a66fccdc4675641a208189d17a
ikirtvardhansingh <ikirtivardhansingh@gmail.com>
2026-06-04 20:31:44 +0530

    P2 section 3: feature presentation

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const loadData = require("./src/loadData");
 const analyzeData = require("./src/eda");
+const preprocessData = require("./src/preprocess");
 
 async function main(){
     const data = await loadData("./data/WA_Fn-UseC_-Telco-Customer-Churn.csv");
 
     console.log(data[0]);
     console.log(data.length);
-  analyzeData(data);
-    
+    analyzeData(data);
+    const result = preprocessData(data);
+     console.log(result.x[0]);
+     console.log(result.y[0]);
+
   }
   main();
